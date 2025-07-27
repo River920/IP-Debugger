ee4399208d00299f4cf555d3044a1d6f52705434
River Manoukian <river.m.phoenix@gmail.com>
2025-07-27 15:46:51 +1000

    Updated code by removing //log and fixing the ip examples

--- ERR63-IP-Debugger/Program.cs
+++ ERR63-IP-Debugger/Program.cs
@@ -1,19 +1,15 @@
-﻿//usings go here
-using System.IO;
+﻿using System.IO;
+//Hello(especially to seb :)))
+///////////////////////
+///////////////////////
 
-//log db area
 Directory.CreateDirectory("ECODES");
 File.WriteAllText(@"ECODES\E_CODES.db", "0 - Success\n1 - General Error\n(0) Database Created Successfully UTC: " + DateTime.UtcNow + "\n\n\n\n");
-
-
-
-//log db area
 
 ///////////////////////
 ///////////////////////
 
 Console.WriteLine("(0)");
-//log
 File.AppendAllText(@"ECODES\E_CODES.db", "(0) Application Running\n");
 Thread.Sleep(2000);
 
@@ -28,13 +24,13 @@ string Start_ID1 = Console.ReadLine();
 
 if (Start_ID1 == "y")
 {
-    //log
+    
     File.AppendAllText(@"ECODES\E_CODES.db", "(0) IP-Debug y/n Valid Input " + Start_ID1 + "\n");
 }
 else if (Start_ID1 == "n")
 {
     Console.WriteLine("(0)");
-    //log
+    
     File.AppendAllText(@"ECODES\E_CODES.db", "(0) IP-Debug y/n Soft Exit\n");
     Thread.Sleep(1500);
     Environment.Exit(0);
@@ -42,10 +38,10 @@ else if (Start_ID1 == "n")
 else
 {
     Console.WriteLine("(1)");
-    //log
+    
     File.AppendAllText(@"ECODES\E_CODES.db", "(1) IP-Debug y/n Invalid Input " + Start_ID1 + "\n");
     Thread.Sleep(1500);
-    //log
+    
     File.AppendAllText(@"ECODES\E_CODES.db", "(0) IP-Debug y/n Soft Exit\n");
     Environment.Exit(0);
 }
@@ -56,7 +52,7 @@ else
 Thread.Sleep(2000);
 
 Console.WriteLine("(0)");
-//log
+
 File.AppendAllText(@"ECODES\E_CODES.db", "(0) IP-Debug On\n");
 Console.ForegroundColor = ConsoleColor.Red;
 
@@ -77,8 +73,8 @@ Thread.Sleep(1000);
 ///////////////////////
 
 Console.ForegroundColor = ConsoleColor.White;
-//log
+
 File.AppendAllText(@"ECODES\E_CODES.db", "(0) Waiting for input on IP type\n");
 Console.WriteLine("(0)");
-Console.WriteLine("Enter the IP type that you want to connect to:\n1 > Device IP < IPv4 > 123.456.7.89\n2 > Server IP < IPv4 > 1.2.3.4\n3 > Network IP < IPv4 > 123.456.7.8");
+Console.WriteLine("Enter the IP type that you want to connect to:\nIPv4 > X.X.X.X < X = 0-255\nIPv6 > XXXX:XXXX:XXXX:XXXX:XXXX:XXXX:XXXX:XXXX < X = 0-9/A-F");
 var ID_IP = Console.ReadLine();
